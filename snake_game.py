bf7e543fb7e420da851feb2111d7a89fd3abe089
Irina <irina.pashchenko@bostongene.com>
2022-05-02 15:54:12 +0200

    biting body

--- snake_game.py
+++ snake_game.py
@@ -12,8 +12,8 @@ class Food:
         self.nutrition = self.create_food()
 
     def create_food(self):
-        x = random.randrange(20, self.width_w // 10) * 10
-        y = random.randrange(20, self.height_w // 10) * 10
+        x = random.randrange(2, self.width_w // 10 - 10) * 10
+        y = random.randrange(2, self.height_w // 10 - 10) * 10
         print(x, y)
         return self.canvas.create_rectangle(x, y, x + 10, y + 10, fill='red')
 
@@ -26,7 +26,7 @@ class Snake:
         self.canvas = canvas
         self.width_w = width_w
         self.height_w = height_w
-        head = self.canvas.create_rectangle(self.width_w/2, self.height_w/2, self.width_w/2 + 10, self.height_w/2 + 10, fill='green')
+        head = self.canvas.create_rectangle(self.width_w/10, self.height_w/10, self.width_w/10 + 10, self.height_w/10 + 10, fill='green')
         self.direction = "Left"
         self.snake_body = [head]
 
@@ -45,7 +45,7 @@ class Snake:
 
     def draw_body(self):
         coords = self.get_position(self.snake_body[-1])
-        x,y = 0, 0
+        x, y = 0, 0
         if self.direction == "Left":
             x = coords[2]
             y = coords[1]
@@ -61,38 +61,67 @@ class Snake:
 
         self.create_block(x, y)
 
+    def create_head(self):
+        coords = self.get_head_position()
+        x, y = 0, 0
+        if self.direction == "Left":
+            x = coords[0] - 10
+            y = coords[1]
+        elif self.direction == "Right":
+            x = coords[2]
+            y = coords[1]
+        elif self.direction == "Up":
+            x = coords[0]
+            y = coords[1] - 10
+        elif self.direction == "Down":
+            x = coords[0]
+            y = coords[3]
+
+        head = self.canvas.create_rectangle(x, y, x + 10, y + 10, fill='green')
+        self.snake_body.insert(0, head)
+
     def step(self):
+        self.create_head()
+        self.canvas.delete(self.snake_body[-1])
+        self.snake_body.pop(-1)
 
         if len(self.snake_body) > 1:
-            for i in range(len(self.snake_body)-1, 0, -1):
-                coords = self.get_position(self.snake_body[i-1])
-                block = self.snake_body[i]
-                self.canvas.moveto(block, coords[0]-1, coords[1]-1)
+            coords = self.get_position(self.snake_body[1])
+            self.canvas.delete(self.snake_body[1])
+            self.snake_body[1] = self.canvas.create_rectangle(coords[0], coords[1], coords[2], coords[3], fill='white')
 
-        if self.direction == "Left":
-            self.canvas.move(self.snake_body[0], -10, 0)
-        elif self.direction == "Right":
-            self.canvas.move(self.snake_body[0], +10, 0)
-        elif self.direction == "Up":
-            self.canvas.move(self.snake_body[0], 0, -10)
-        elif self.direction == "Down":
-            self.canvas.move(self.snake_body[0], 0, +10)
-
+    def check_biting(self):
+        coords = self.get_head_position()
+        if len(self.snake_body) > 3:
+            for block in self.snake_body[3:-1]:
+                coords_block = self.get_position(block)
+                if coords[0] == coords_block[0] and \
+                    coords[1] == coords_block[1] and \
+                    coords[2] == coords_block[2] and \
+                    coords[3] == coords_block[3]:
+                    print(coords)
+                    print(coords_block)
+                    return True
 
 
 class Model:
     canvas = None
-    width_w = 600
-    height_w = 400
+    width_w = 400
+    height_w = 300
 
     def __init__(self, root):
+        root.geometry('400x340')
+        root.configure(background="white")
         self.canvas = tk.Canvas(root, width=self.width_w, height=self.height_w)
-        self.canvas.pack()
+        exit_button = tk.Button(root, text="Exit", command=root.destroy)
+        exit_button.place(x=5, y=310)
+        self.canvas.pack(side=tk.TOP)
         self.game = True
         self.snake = Snake(self.canvas, self.width_w, self.height_w)
         self.food = Food(self.canvas, self.width_w, self.height_w)
         x = threading.Thread(target=self.cycle_move)
         x.start()
+        self.score = 0
 
     def cycle_move(self):
         while self.game is True:
@@ -105,20 +134,22 @@ class Model:
         coords = self.snake.get_head_position()
         if coords[0] < 0 or coords[1] < 0 or coords[2] > self.width_w or coords[3] > self.height_w:
             self.game_over()
+        if self.snake.check_biting() is True:
+            self.game_over()
 
     def game_over(self):
         self.snake.kill_snake()
         self.game = False
-        self.canvas.create_text(300, 200, text='You Lose. Game Over.')
+        self.canvas.create_text(self.width_w/2, self.height_w/2, text='You Lose. Game Over.')
 
     def check_food(self):
         coord_food = self.food.get_position()
         coord_snake = self.snake.get_head_position()
         if [coord_food[0], coord_food[1]] == [coord_snake[0], coord_snake[1]]:
+            self.score += 1
             self.snake.draw_body()
             self.canvas.delete(self.food.nutrition)
             self.food.nutrition = self.food.create_food()
-
 
 
 class Controller:
